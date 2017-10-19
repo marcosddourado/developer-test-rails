6a47b1dcbfea89c739dec9a19140ac8fa2f39146
Marcos Dourado <marcosdourado92@gmail.com>
2017-10-19 14:23:53 -0200

    testing method 'edit'

--- src/t1/paginasweb/test/controllers/webpages_controller_test.rb
+++ src/t1/paginasweb/test/controllers/webpages_controller_test.rb
@@ -28,30 +28,22 @@ class WebpagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get edit" do
+    get edit_webpage_url(@webpage)
+    assert_response :success
+  end
+
 
   # test "should get update" do
   #   get webpages_update_url
   #   assert_response :success
   # end
 
-  # test "should get edit" do
-  #   get webpages_edit_url
-  #   assert_response :success
-  # end
 
   # test "should get destroy" do
   #   get webpages_destroy_url
   #   assert_response :success
   # end
 
-  # test "should get index" do
-  #   get webpages_index_url
-  #   assert_response :success
-  # end
-
-  # test "should get show" do
-  #   get webpages_show_url
-  #   assert_response :success
-  # end
 
 end
